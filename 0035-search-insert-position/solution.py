0b603bb0bd62a6ffcf2f5919ccb712d8d206f99a
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-10-11 16:23:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.3 MB (57.83%)

--- 0035-search-insert-position/solution.py
+++ 0035-search-insert-position/solution.py
@@ -1,18 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # Using binary search
-        # Perform binary search 
-        # If not found return the index it should be in
+        
+        l, r = 0, len(nums)
 
-        l, r = 0, len(nums)-1
-
-        while l <= r:
-            mid = (l+r)//2
-            if nums[mid] > target:
-                r = mid - 1
-            elif nums[mid] < target:
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] < target:
                 l = mid + 1
             else:
-                return mid
+                r = mid
 
         return l
