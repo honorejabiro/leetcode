70dacb1c2881251e52fc8c2c81ab75d0220b8099
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-03 20:41:09 +0000

    Sync LeetCode submission Runtime - 103 ms (56.75%), Memory - 28.4 MB (72.57%)

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -1,24 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        start = 0
-        end = len(height) - 1
-        area = 0
-
-        while start < end:
-            start_line = height[start]
-            end_line = height[end]
-            smaller = start_line
-
-            if start_line > end_line:
-                smaller = end_line
-
-            curr_area = (end - start) * smaller
-            if area < curr_area: 
-                area = curr_area
-
-            if start_line > end_line:
-                end -= 1
+        # Create a res integer 
+        # Starting from the ends of the container
+        # While left pointer is less than right pointer
+        # Find the current area of water contained and set to max
+        # Move the ppointer with lowest height
+        res = float("-inf")
+        l, r = 0, len(height) - 1
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            res = max(res, area)
+            if height[l] > height[r]:
+                r -= 1
             else:
-                start += 1
-
-        return area
+                l += 1
+        return res
