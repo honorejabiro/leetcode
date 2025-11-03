4fa764f273502cbd01b5d641b5e9868a96f22731
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-03 18:51:43 +0000

    Sync LeetCode submission Runtime - 5 ms (34.34%), Memory - 18.6 MB (53.70%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.py
+++ 0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-
+        # Using binary search
+        # Create a left and right pointer
+        # While left is less or equal to right
+        # Find the middle number
+        # Move the pointers according to the middle 
+        # If the total is target return a list of left and right
         l, r = 0, len(numbers) - 1
-
-        while l < r:
-
-            if numbers[l] + numbers[r] == target:
-                return [l + 1, r + 1]
-
-            elif numbers[l] + numbers[r] < target:
+        while l <= r:
+            tot = numbers[l] + numbers[r]
+            if tot == target:
+                return [l+1, r+1]
+            elif tot > target:
+                r -= 1
+            elif tot < target:
                 l += 1
 
-            else:
-                r -= 1
-
+        
