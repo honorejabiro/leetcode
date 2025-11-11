a582e0ed23b4d627ca7a056f6238478221a6a629
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-11 05:10:08 +0000

    Sync LeetCode submission Runtime - 175 ms (18.56%), Memory - 77.8 MB (89.43%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,54 +1,48 @@
 class ListNode:
-
-    def __init__(self, key, val):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
         self.prev = None
         self.next = None
-
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}  # key : node
+        self.cache = {}
 
-        # Left = LRU and Right = MRU
         self.left = ListNode(0, 0)
         self.right = ListNode(0, 0)
-        self.left.next, self.right.prev = (
-            self.right,
-            self.left,
-        )  # They point at each other initally
 
+        self.left.next, self.right.prev = self.right, self.left
+    
     def remove_from_list(self, node: Optional[ListNode]) -> None:
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
     def insert_in_list(self, node: Optional[ListNode]) -> None:
         right = self.right
-        mru = self.right.prev
+        mru = right.prev
         mru.next, node.prev = node, mru
-        node.next, self.right.prev = right, node
+        self.right.prev, node.next = node, right
 
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
             self.remove_from_list(node)
             self.insert_in_list(node)
-            return node.val
-
+            return node.value
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-
         if key in self.cache:
             node = self.cache[key]
             self.remove_from_list(node)
-            node.val = value
+            node.value = value
             self.insert_in_list(node)
             self.cache[key] = node
-
+        
         else:
             new_node = ListNode(key, value)
             self.cache[key] = new_node
@@ -58,10 +52,10 @@ class LRUCache:
                 lru = self.left.next
                 self.remove_from_list(lru)
                 del self.cache[lru.key]
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
