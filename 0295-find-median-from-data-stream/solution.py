09ae12d1209114e1596d90308d9edbe1d9bb3f34
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-12 14:32:16 +0000

    Sync LeetCode submission Runtime - 264 ms (14.21%), Memory - 41.3 MB (10.89%)

--- 0295-find-median-from-data-stream/solution.py
+++ 0295-find-median-from-data-stream/solution.py
@@ -1,40 +1,31 @@
 class MedianFinder:
 
     def __init__(self):
-        self.small = [] # max heap
-        self.large = [] # min hea
-        heapq.heapify(self.small)
-        heapq.heapify(self.large)
+        self.left = [] # small
+        self.right = [] # large
 
     def addNum(self, num: int) -> None:
-        
-        # Add to the small heap
-        heapq.heappush(self.small, -num)
-        
-        # Balance both heaps
-        largest_left = heapq.heappop(self.small) 
-        heapq.heappush(self.large, -largest_left)
+        # Push to small heap
+        heapq.heappush(self.left, -num)
+
+        # Push largest left to large heap
+        largest_left = heapq.heappop(self.left)
+        heapq.heappush(self.right, -largest_left)   
 
         # Maintain size property
-        if len(self.small) < len(self.large):
-            smallest_right = heapq.heappop(self.large)
-            heapq.heappush(self.small, -smallest_right)
-
+        if len(self.left) < len(self.right):
+            smallest_right = heapq.heappop(self.right)
+            heapq.heappush(self.left, -smallest_right)
 
     def findMedian(self) -> float:
-        
-        m = len(self.small)
-        n = len(self.large)
 
-        if m == n:
-            median = (-self.small[0] + self.large[0]) / 2
+        if len(self.left) == len(self.right):
+            m1 = -self.left[0]
+            m2 = self.right[0]
+            return (m1 + m2) / 2
         
         else:
-            median = -self.small[0]
-        
-        return median
-        
-
+            return -self.left[0]
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
