84e5d7879145fe3f675d9d25fc5b54baa222a422
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-13 15:38:59 +0000

    Sync LeetCode submission Runtime - 5 ms (53.07%), Memory - 17.9 MB (15.84%)

--- 0013-roman-to-integer/solution.py
+++ 0013-roman-to-integer/solution.py
@@ -1,21 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+
         hashmap = {
-            "I" :      1,
-            "V" :       5,
-            "X" :       10,
-            "L" :       50,
-            "C" :       100,
-            "D" :       500,
-            "M" :       1000
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
         }
-        tot = 0
-        i = 0
-        while i < len(s):
-            if i <= (len(s) - 2) and hashmap[s[i]] < hashmap[s[i+1]]:
-                tot += hashmap[s[i+1]] - hashmap[s[i]]
-                i += 2
+
+        n = len(s)
+        i = n - 1
+        answer = hashmap[s[i]]
+
+        while i > 0:
+            if hashmap[s[i]] <= hashmap[s[i - 1]]:
+                answer += hashmap[s[i - 1]]
             else:
-                tot += hashmap[s[i]]
-                i += 1
-        return tot
+                answer -= hashmap[s[i - 1]]
+            i -= 1
+
+        return answer
+
