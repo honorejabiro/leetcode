d30c178274c75a71c7c347aba9eb01351e193e4d
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-18 21:42:03 +0000

    Sync LeetCode submission Runtime - 231 ms (14.32%), Memory - 18 MB (39.63%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        
+        n = len(s)
+        if n == 0:
+            return 0
 
-        length = len(s)
+        counter = defaultdict(int)
         l, r = 0, 0
         max_freq = 0
         longest = 0
-        hashmap = {chr(i): 0 for i in range(65, 91)}
 
-        while r < length:
-            hashmap[s[r]] += 1
+        for r in range(n):   
+            counter[s[r]] += 1
             window_size = r - l + 1
-            max_freq = max(max_freq, hashmap[s[r]])
+            max_freq = max(max_freq, counter[s[r]])
+     
 
-            if window_size - max_freq <= k:
-                longest = max(longest, window_size)
-
-            else:
-                hashmap[s[l]] -= 1
+            if window_size - max_freq > k:
+                counter[s[l]] -= 1
                 l += 1
 
-            r += 1
+            window_size = r - l + 1
+            longest = max(longest, window_size)
 
+        
         return longest
 
+
+    
