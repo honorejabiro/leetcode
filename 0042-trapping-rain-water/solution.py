6972ddbea2a3334ae057737bbf0750a46b599e72
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-18 23:01:29 +0000

    Sync LeetCode submission Runtime - 15 ms (51.60%), Memory - 19.3 MB (59.65%)

--- 0042-trapping-rain-water/solution.py
+++ 0042-trapping-rain-water/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
+        
+        n = len(height)
+
+        l, r = 0, n - 1
         max_left, max_right = 0, 0
-        area = 0
+        trapped = 0
+
         while l < r:
             if height[l] < height[r]:
-                add = max_left - height[l]
-                if add > 0:
-                    area += add
-                max_left = max(max_left, height[l])
+                max_left = max(height[l], max_left)
+                trapped += max_left - height[l]
                 l += 1
             else:
-                add = max_right - height[r]
-                if add > 0:
-                    area += add
-                max_right = max(max_right, height[r])
+                max_right = max(height[r], max_right)
+                trapped += max_right - height[r]
                 r -= 1
-
-        return area
+        
+        return trapped
