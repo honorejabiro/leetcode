4ee485351c08039b84682cc2de1ab908998ffa66
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-11-18 19:31:15 +0000

    Sync LeetCode submission Runtime - 793 ms (43.99%), Memory - 18 MB (78.06%)

--- 0322-coin-change/solution.py
+++ 0322-coin-change/solution.py
@@ -1,22 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Using Tabulation or Bottom up
-        # Create a dp of length 11
-        # Edge case 0
-        # Starting from 1 Find the remainder and take the minimum
-        # Reach the end and return the last index value if it not infinity
-
-        dp = [float("inf")] * (amount+1)
+        
+        n = len(coins)
+        dp = [float("inf")] * (amount + 1)
         dp[0] = 0
 
-        for i in range(1, amount+1):
-            for j in coins:
-                if j <= i:
-                    dp[i] = min(dp[i], dp[i-j] + 1)
-
-        print(dp)
-
-        if dp[-1] == float("inf"):
-            return -1
-
-        return dp[-1]
+        for i in range(n):
+            for curr_amount in range(amount + 1):
+                if curr_amount >= coins[i]:
+                    dp[curr_amount] = min(dp[curr_amount], 1 + dp[curr_amount - coins[i]])
+        
+        return dp[-1] if dp[-1] != float("inf") else -1
