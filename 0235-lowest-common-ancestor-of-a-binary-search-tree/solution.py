9e605a00dfd437b25ea58274ae0b9e6eeeb30a10
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-08 20:33:23 +0000

    Sync LeetCode submission Runtime - 61 ms (35.77%), Memory - 21.2 MB (15.98%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -4,19 +4,16 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+from collections import deque
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(root):
-            if root is None:
-                return
-            if root.val > q.val and root.val > p.val:
-                return dfs(root.left)
-
-            elif root.val < q.val and root.val < p.val:
-                return dfs(root.right)
-
+         curr = root
+         while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
             else:
-                return root
+                return curr
 
-        return dfs(root)
+
