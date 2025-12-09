5623f11b5c30ee7635c01d0d6e5e51cbae10ad7f
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-09 19:48:19 +0000

    Sync LeetCode submission Runtime - 1 ms (43.49%), Memory - 18 MB (56.47%)

--- 0022-generate-parentheses/solution.py
+++ 0022-generate-parentheses/solution.py
@@ -1,24 +1,33 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
-        answer = []
+        # Base case if the closing is greater than opening, if the opeing or clsing is greater than n
+        # Define the result list
+        # Define the dfs function
+        # If the the length of the divided by 2 is equal to n add the path copy to the result
+        # Add the opening bracket to the path
+        # Recurse the function
+        # After backtracking
+        # Pop from the list
+        # Add the closing bracket to the path
+        # Call the function 
+        # Return the result list
+        result = []
 
-        def f(length, opening, closing, path):
-
-            if length == 2 * n:
-                answer.append("".join(path))
+        def dfs(path, opening, closing):
+            if closing > opening or closing > n or opening > n:
                 return
-            
-            if opening < n:
-                path.append("(")
-                f(length + 1, opening + 1, closing, path)
-                path.pop()
-            
-            if opening > closing:
-                path.append(")")
-                f(length + 1, opening, closing + 1, path)
-                path.pop()
-        
-        f(0, 0, 0, [])
-        return answer
+
+            if len(path)/2 == n:
+                result.append(''.join(path.copy()))
+                return
+
+            path.append("(")
+            dfs(path, opening+1, closing)
+            path.pop()
+            path.append(")")
+            dfs(path, opening, closing+1)
+            path.pop()
+
+        dfs([], 0, 0)
+        return result
 
