f2d96da4bf75c4ca08b93db522312727d81a0a2e
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-09 15:42:27 +0000

    Sync LeetCode submission Runtime - 103 ms (22.09%), Memory - 89.3 MB (16.80%)

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -6,45 +6,15 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        """
-        - preorder = [3,9,20,15,7]
-        - inorder = [9,3,15,20,7]
-
-        - start with 3 (first in preorder)
-        - find the index of 3 in the inorder array
-        - i = 0
-        - l, r = 0, 4
-        - [3].left = [9]
-        - [3].right = [15, 20, 7]
-
-        - i = 1
-        - l, r = 0, 1
-        - [9]
-
-        - i = 3
-        - [20]
-        - l, r = 1, 4
-        """
-
-        inorder_indices = {x: i for i, x in enumerate(inorder)}
-        n = len(preorder)
-        i = 0
-
-        def f(left, right):
-
-            nonlocal i
-            if i >= n:
+        def rec(pre, ino):
+            if not pre or not ino:
                 return None
 
-            if left == right:
-                return None
-
-            inorder_idx = inorder_indices[preorder[i]]
-            node = TreeNode(inorder[inorder_idx])
-            i += 1
-            node.left = f(left, inorder_idx)
-            node.right = f(inorder_idx + 1, right)
+            node = TreeNode(pre[0])
+            position = ino.index(pre[0])
+            node.left = rec(pre[1:position+1], ino[:position])
+            node.right = rec(pre[position+1:], ino[position+1:])
             return node
 
-        return f(0, n)
-
+        return rec(preorder, inorder)
+        
