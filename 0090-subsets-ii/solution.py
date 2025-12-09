a0b0f52ea9dd4a65fd8ee4952afb3da5b18a747f
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-09 16:53:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (44.56%)

--- 0090-subsets-ii/solution.py
+++ 0090-subsets-ii/solution.py
@@ -1,20 +1,26 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # Sort the list
+        # Create the dfs function
+        # Store the path to the result
+        # If the starting index is same as length return
+        # Skip duplicates and operate the recurse
+        # Backtrack and pop the number from the path
         nums.sort()
-        length = len(nums)
         result = []
-        def backtrack(starting_index, path):
-            result.append(path.copy())
-
-            if starting_index == length:
+        length = len(nums)
+        def dfs(starting_index, path):
+            result.append(path[:])
+            if length == starting_index:
                 return
 
             for i in range(starting_index, length):
-                if i > starting_index and nums[i] == nums[i - 1]:
+                if i > starting_index and nums[i-1] == nums[i]:
                     continue
+
                 path.append(nums[i])
-                backtrack(i+1, path)
+                dfs(i+1, path)
                 path.pop()
 
-        backtrack(0, [])
+        dfs(0, [])
         return result
