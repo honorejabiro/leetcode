7039f716791e5c3667edace82ef3ce7f6fb2e958
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-11 18:56:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.3 MB (76.66%)

--- 0035-search-insert-position/solution.py
+++ 0035-search-insert-position/solution.py
@@ -1,13 +1,22 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        l, r = 0, len(nums)
-
-        while l < r:
-            mid = l + (r - l) // 2
-            if nums[mid] < target:
+        # Get pointers left and right
+        # While left is less than right
+        # Find the mid
+        # If the mid index value is target return mid
+        # If it less update the left to mid+1
+        # If it is greater than the target update the right to mid-1
+        # If the value is not found return left
+        if len(nums) == 0:
+            return -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
                 l = mid + 1
-            else:
-                r = mid
-
+                
         return l
