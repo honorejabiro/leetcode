1932545d6b024e78a6ed238f118f392e34b499d0
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-11 18:45:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (54.48%)

--- 0017-letter-combinations-of-a-phone-number/solution.py
+++ 0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,36 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # Create a hashmap that stores list of all possible letters
-        # Create the result variable
-        # Identify the dfs function 
-        # If the Starting_index has reached the end add the path to the result
-        # Create a for loop for all possible paths at current index
-        # Add them to the path and recursively call the function
-        # After backtracking pop from the list
+        # Base is the length of the string
+        # Create a hashmap mapping the integer to the strings
+        # # Using index to keep track of our limit
         if len(digits) == 0:
             return []
+        res = []
+        length = len(digits)
         hashmap = {
-            "1": "",
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
         }
-        result = []
-        n = len(digits)
-        def dfs(starting_index, path):
-            if starting_index == n:
-                result.append("".join(path.copy()))
-                return
 
-            for i in hashmap[digits[starting_index]]:
+        def dfs(index, path):
+            if index == length:
+                res.append("".join(path[:]))
+                return 
+
+            for i in hashmap[str(digits[index])]:
                 path.append(i)
-                dfs(starting_index+1, path)
+                dfs(index+1, path)
                 path.pop()
-        
+
         dfs(0, [])
-        return result
+        return res
