c48341bb294fd3b590ac90d15658a3af7999a6ef
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-11 01:58:28 +0000

    Sync LeetCode submission Runtime - 5516 ms (17.29%), Memory - 17.6 MB (79.65%)

--- 0079-word-search/solution.py
+++ 0079-word-search/solution.py
@@ -1,60 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # Base case if the word is greater than the word target, if the path is equal to the word change the result variable to True
-        # Create a visited list
-        # We need a visited data structure for our cell iteration
-        # We need a visited set for our dfs function parameters: cell, grid
-        # Define a neighbors function
-        # Create a truth variable default False
-        # Get the neighbors
-        # Itearte over the returned moves
-        # Add the move to the path 
-        # Pop from the path
-        length = len(word)
-        answer = False
-        truth = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-
-        def get_neighbors(cell, grid):
-            new_r = [-1, 0, 1, 0]
-            new_c = [0, 1, 0, -1]
-            moves = []
-
-            for i in range(4):
-                nr = cell[0] + new_r[i]
-                nc = cell[1] + new_c[i]
-                
-                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                    moves.append((nr, nc))
-
-            return moves
-        
-        def dfs(cell, grid, path, truth):
-            nonlocal answer
-            if answer:
-                return
-            r0, c0 = cell
-            path.append(grid[r0][c0])
-            truth[r0][c0] = True
-
-            if len(path) == length:
-                if ''.join(path) == word:
-                    answer = True
-                truth[r0][c0] = False
-                path.pop()
-                return
-            for r, c in get_neighbors(cell, grid):
-                if truth[r][c]:
-                    continue
-                if grid[r][c] == word[len(path)]:
-                    dfs((r, c), grid, path, truth)
-            truth[r0][c0] = False
-            path.pop()
+        def dfs(cell, grid, index, visited):
+            nonlocal word
+            r, c = cell[0], cell[1]
+            if index == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != word[index]) or (r, c) in visited:
+                return False
+            visited.add((r, c))
+            res =  (dfs((r-1, c), grid, index+1, visited) or
+                dfs((r, c+1), grid, index+1, visited) or
+                dfs((r+1, c), grid, index+1, visited) or
+                dfs((r, c-1), grid, index+1, visited))
+            visited.remove((r, c))
+            return res
+            
 
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if answer:
-                    return True
-                if board[r][c] == word[0]:   
-                    dfs((r, c), board, [], truth)
+                if board[r][c] == word[0]:
+                    if dfs((r,c), board, 0, set()):
+                        return True
 
-        return answer
+        return False
