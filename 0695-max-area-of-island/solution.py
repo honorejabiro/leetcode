252d6ee013ba82003514dbf9d84f95880a36b8dc
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-17 06:06:43 +0000

    Sync LeetCode submission Runtime - 57 ms (6.17%), Memory - 18.6 MB (30.07%)

--- 0695-max-area-of-island/solution.py
+++ 0695-max-area-of-island/solution.py
@@ -1,43 +1,41 @@
 from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if len(grid) == 0:
-            return 0
-        
+        res = 0
+        rows = len(grid)
+        cols = len(grid[0])
         visited = set()
-        answer = 0
-        
-        rows, columns = len(grid), len(grid[0])
-        
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        
-        def bfs(row,column):
-            visited.add((row,column))
-            area = 0
-            queue = deque([(row,column)])
-            
-            while queue:
-                r, c = queue.popleft()
-                area += 1
-                
-                for direction_row, direction_column in directions:
-                    current_row = r + direction_row
-                    current_column = c + direction_column
+        def get_neighbors(cell, grid):
+            moves = []
+            dr = [1, 0, -1, 0]
+            dc = [0, 1, 0, -1]
+            r, c = cell[0], cell[1]
+
+            for i in range(4):
+                nr, nc = dr[i] + r, dc[i] + c
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
+                    moves.append((nr, nc))
+            print(moves)
+
+            return moves
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    q = deque([(r, c)])
+                    visited.add((r,c))
+                    area = 0
+                    while q:
+                        length = len(q)
+                        for i in range(length):
+                            node = q.popleft()
+                            area += 1
+                            for move in get_neighbors(node, grid):
+                                if move not in visited:
+                                    visited.add(move)
+                                    q.append(move)
+                            
                     
-                    if  (
-                        (current_row in range(rows)) and
-                        (current_column in range(columns)) and
-                        (grid[current_row][current_column] == 1) and
-                        ((current_row,current_column) not in visited)
-                        ):
-                        visited.add((current_row,current_column))
-                        queue.append((current_row,current_column))
-                        
-            return area
-                
-        for row in range(rows):
-            for column in range(columns):
-                if((row, column) not in visited) and (grid[row][column] == 1):
-                    answer = max(answer,bfs(row,column))
-                    
-        return answer
+                    res = max(res, area)
+
+        return res
