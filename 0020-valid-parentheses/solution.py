ed7fa7b870f2986cbc97292217779fb7ff5a7c61
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-21 23:36:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.4 MB (93.44%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,26 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Create a hashmp to represent the closing brckets
-        # Create a stack
-        # Iterate over the string
-        # If the string is closing Check the stack last element if it is different return False
-        # Add the string to the stack
-        # Return True if the stack is empty
-        hashmap = {
-            "]": "[",
-            ")": "(",
-            "}": "{"
+        closing = {
+            ')': '(',
+            '}': '{',
+            ']': '[' 
         }
-
         stack = []
-        for i in s:
-            if i in hashmap:
-                if len(stack) == 0 or stack[-1] != hashmap[i]:
-                    return False
-                stack.pop()
 
+        for i in s:
+            if i in closing:
+                if stack:
+                    if stack.pop() != closing[i]:
+                        return False
+                else:
+                    return False
             else:
                 stack.append(i)
 
         return len(stack) == 0
-
