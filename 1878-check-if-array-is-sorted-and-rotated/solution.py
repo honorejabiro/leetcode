ccc70e678e3b2bade786101264216ee6c3c366eb
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-22 07:08:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (97.26%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.py
+++ 1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -1,8 +1,17 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] > nums[(i+1)%len(nums)]:
-                count += 1
+        moved = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                moved = i
+        i = (moved + 1) % len(nums)
 
-        return count <= 1
+        while i%len(nums) != moved:
+            if nums[(i)%len(nums)] < nums[(i-1)%len(nums)]:
+                return False
+
+            i += 1
+
+
+        return True
+
