716c54b6a20a67016dccd1cf58a2cd56a82afdbc
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-23 23:30:05 +0000

    Sync LeetCode submission Runtime - 3 ms (8.00%), Memory - 17.2 MB (97.09%)

--- 0198-house-robber/solution.py
+++ 0198-house-robber/solution.py
@@ -1,19 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        for i in range(2, len(nums)):
-            curr = nums[i]
-            tot = 0
-            for j in range(0, i-1):
-                if nums[j] + curr > tot:
-                    nums[i] = nums[j] + curr
-                    tot = nums[j] + curr
-                    
-        return max(nums[-1], nums[-2])
+        dp = [None] * len(nums) 
+        def dfs(index):
+            if index == len(nums) - 2:
+                dp[index] = nums[index]
+                return dp[index]
+
+            if index == len(nums) - 1:
+                dp[index] = nums[index]
+                return dp[index]
+
+            if dp[index] is not None:
+                return dp[index]
+
+            ans = nums[index]
+            dp[index] = ans
+
+            for i in range(index+2, len(nums)):
+                dp[index] = max(dp[index], (ans + dfs(i)))
+            return dp[index]
+
+        ans = 0
+
+        for i in range(len(nums)):
+            ans = max(ans, dfs(i))
+
+        return ans
