d3273b78436dd9f267a02c9f5149694a2cffad0b
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-26 20:18:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (96.19%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # Turn the numbers into a string
-        # Turn the string into an integer
-        # Add one to the integer
-        # Turn the digit into a string then back to the int
         for i in range(len(digits)):
             digits[i] = str(digits[i])
-        strs = "".join(digits)
-        num = int(strs)
-        num += 1
-        nums = str(num)
+        w = "".join(digits)
+        number = int(w)
+        number += 1
+        w = str(number)
         res = []
-        for i in nums:
+        for i in w:
             res.append(int(i))
 
         return res
+
+        
