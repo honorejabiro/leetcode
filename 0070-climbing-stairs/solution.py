d2943c1f6ba8618295e0253cf64b10630724e3ed
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2025-12-26 20:36:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (95.81%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,16 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Tabulation method
-        # A dp array of number of steps
-        # First and second indices are 0 and 1 respectively
-        # Starting from the third index find the sum of ways by  adding the sum of ways from 1 step back and 2 steps back
-        # Return the last index in the array
-        dp = [0] * (n+1)
-        dp[1], dp[0] = 1, 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
 
         for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-
+            dp[i] = dp[i-1] + dp[i-2] 
         return dp[-1]
-
