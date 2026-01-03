eefb678d12bf4b89b608ddb8a5e7cfb962508c15
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2026-01-03 17:34:00 +0000

    Sync LeetCode submission Runtime - 15 ms (16.58%), Memory - 17.5 MB (91.20%)

--- 0965-unique-email-addresses/solution.py
+++ 0965-unique-email-addresses/solution.py
@@ -1,28 +1,29 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        number_of_emails = {}
+        # Create a function that checks the local name
+        # Create a hashmap
+        # Iterate over the list of strings
+        # Change the domain name
+        # Add the string to the. set
+        # Return the number of the lenght of the set
+
+        def local(local):
+            res = []
+            for i in local:
+                if i == '+':
+                    break
+                if i == '.':
+                    continue
+                res.append(i)
+
+            return ''.join(res)
+
+        s = set()
+
         for email in emails:
-            email_components = email.split("@")
-            
-            local = email_components[0]
-            domain = email_components[1]
-            
-            new_local = ""
-            
-            instance_of_plus = local.find("+")
-            
-            if instance_of_plus != -1:
-                new_local = local[0:instance_of_plus]
-            else:
-                new_local = local
-                
-            new_local = new_local.replace(".", "")
-            
-            trimmed_email = new_local + "@" + domain
-            
-            if trimmed_email not in number_of_emails:
-                number_of_emails[trimmed_email] = 0
-            else:
-                number_of_emails[trimmed_email] += 1
-        return len(number_of_emails)
-                
+            index = email.index('@')
+            new = local(email[:index])
+            forward = new + email[index:]
+            s.add(forward)
+
+        return len(s)
