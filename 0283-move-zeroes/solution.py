5f817a41fdcb048e9e165fb77dd40bb30c680792
Honore Jabiro <150479192+honorejabiro@users.noreply.github.com>
2026-01-03 16:38:34 +0000

    Sync LeetCode submission Runtime - 7 ms (42.56%), Memory - 18.7 MB (77.57%)

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -3,31 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # [1,3,0,0,0,12]
+        # Create a left and right pointer
+        # Both pointers start at index 0
+        # If pointer left is 0 don't move it
+        # Move right and if right is a number swap if left is 0
+        # Move right 
 
-        # Create pointers left and right
-        # Edge case; length of array less than 2 return array
-        # Move until r is at the end
-        # If right is a number and left is 0 swap the two and shift both by 1
-        # If none of them is 0 move both by 1
-        # If both are 0 shift just the right
-
-        l, r = 0, 1
-
+        l, r = 0, 0
         while r < len(nums):
-            if nums[r] != 0 and nums[l] == 0:
+            if nums[l] == 0 and nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
                 r += 1
-                continue
-            elif (nums[r] != 0 and nums[l] != 0) or (nums[r] == 0 and nums[l] != 0):
+
+            elif nums[l] == 0 and nums[r] == 0:
+                r += 1
+            
+            elif nums[l] != 0 and nums[r] != 0:
                 l += 1
                 r += 1
-                continue
-                
-            else:
-                r += 1
 
-        return nums
 
-        
+            
